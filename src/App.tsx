b298535f6ceb8326455467a6ae5f99bfd0c8f12d
Marina <mareh.ps@gmail.com>
2024-05-16 17:49:08 -0300

    adding ayout with ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import { Col, Row } from 'antd';
+import { Flex, Menu } from "antd";
+import Sider from "antd/es/layout/Sider";
+import Layout, { Content, Footer, Header } from "antd/es/layout/layout";
+import Title from "antd/es/typography/Title";
+
+const items = new Array(15).fill(null).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: "hidden",
+  backgroundColor: "#f4f4f4",
+};
+
+const headerStyle = {
+  height: "auto",
+  display: "flex",
+  alignItems: "center",
+  
+};
 
 function App() {
   return (
-    <div>
-      <Row>
-        <Col span={8}>
-          aa
-        </Col>
-        <Col span={8}>
-          aa
-        </Col>
-        <Col span={8}>
-          aa
-        </Col>
-      </Row>
+    <div className="app">
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>
+          <Title>Bloom</Title>
 
+          <Menu
+            items={items}
+            mode="horizontal"
+          />
+        </Header>
+
+        <Layout style={layoutStyle}>
+          <Sider
+            style={layoutStyle}
+            width="25%"
+          >
+            Sider
+          </Sider>
+          <Content>Content</Content>
+        </Layout>
+
+        <Footer style={layoutStyle}>footer</Footer>
+      </Layout>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ConfigProvider, ThemeConfig } from "antd";
-
-
-const config: ThemeConfig = {
-  token: {
-    colorPrimary: "#40B3A2",
-  },
-};
+import { ConfigProvider, theme } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConfigProvider theme={config} >
-      <App />
+    <ConfigProvider
+      theme={{
+        algorithm: theme.defaultAlgorithm,
+
+        token: {
+          // Seed Token
+          colorPrimary: "#7A77B9",
+          borderRadius: 30,
+          colorTextBase: "#333",
+          fontSize: 16,
+          fontFamily: "Ubuntu",
+          colorPrimaryBg: "#f45555",
+          colorBgBase: "#f4f4f4"
+        },
+      }}
+    >
+      <App/>
     </ConfigProvider>
   </React.StrictMode>,
 );
